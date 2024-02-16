b17963aa485e2a7e8a24a49952229485efbea347
Siriusxz <114768817+Siriuszx@users.noreply.github.com>
2024-02-16 12:01:33 +0100

    refactor: replace contextApi with RTK

--- src/main.tsx
+++ src/main.tsx
@@ -6,11 +6,16 @@ import { ThemeProvider } from 'styled-components';
 import mainTheme from './styles/MainTheme';
 import GlobalStyles from './styles/GlobalStyles.styled';
 
+import { store } from './app/store';
+import { Provider } from 'react-redux';
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ThemeProvider theme={mainTheme}>
-      <GlobalStyles />
-      <Router />
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={mainTheme}>
+        <GlobalStyles />
+        <Router />
+      </ThemeProvider>
+    </Provider>
   </React.StrictMode>
 );
