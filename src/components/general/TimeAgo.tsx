3522dbff8dc741ca5f1ccd78079ae7fd95d4abe2
siriuszx <114768817+Siriuszx@users.noreply.github.com>
2024-03-03 11:37:25 +0100

    feat: add relative dates

--- src/components/general/TimeAgo.tsx
+++ src/components/general/TimeAgo.tsx
@@ -1,15 +1,25 @@
 import { ReactNode } from 'react';
-import { DateTime } from 'luxon';
+import { DateTime, Duration } from 'luxon';
 
 import { Wrapper } from './TimeAgo.styled';
 
 type PostDateProps = { className?: string; children?: ReactNode; date: string };
 
 const TimeAgo = ({ className, children, date }: PostDateProps) => {
+  const duration: Duration = DateTime.fromJSDate(new Date()).diff(DateTime.fromISO(date));
+
+  const dayDiff: number = duration.as('days');
+  const wholeDaysDiff: number = Math.floor(dayDiff);
+
+  const relative: string = DateTime.fromISO(date).toRelative() as string;
+  const absolute: string = DateTime.fromISO(date).toLocaleString(DateTime.DATE_MED);
+
+  const dateResult = wholeDaysDiff > 10 ? absolute : relative;
+
   return (
     <Wrapper className={className}>
       {children}
-      {DateTime.fromISO(date).toLocaleString(DateTime.DATE_MED)}
+      {dateResult}
     </Wrapper>
   );
 };
