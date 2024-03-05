5e73e1ebacb849d72f4ccc55979f13f323be500b
siriuszx <114768817+Siriuszx@users.noreply.github.com>
2024-03-05 21:29:40 +0100

    refactor: app date package dependency

--- src/components/general/TimeAgo.tsx
+++ src/components/general/TimeAgo.tsx
@@ -1,21 +1,19 @@
 import { ReactNode } from 'react';
-import { DateTime, Duration } from 'luxon';
 
 import { Wrapper } from './TimeAgo.styled';
+import { AppDate } from '@/utils/appDate';
 
 type PostDateProps = { className?: string; children?: ReactNode; date: string };
 
 const TimeAgo = ({ className, children, date }: PostDateProps) => {
-  const duration: Duration = DateTime.fromJSDate(new Date()).diff(DateTime.fromISO(date));
-
-  const diffAsDays = duration.as('days');
-  const diffAsMinutes = duration.as('minutes');
+  const diffAsDays: number = AppDate.getFromNowDiffAs(date, 'days');
+  const diffAsMinutes: number = AppDate.getFromNowDiffAs(date, 'minutes');
 
   const wholeDaysDiff: number = Math.floor(diffAsDays);
   const wholeMinutesDiff: number = Math.floor(diffAsMinutes);
 
-  const relative: string = DateTime.fromISO(date).toRelative() || '';
-  const absolute: string = DateTime.fromISO(date).toLocaleString(DateTime.DATE_MED);
+  const relative: string = AppDate.getRelative(date);
+  const absolute: string = AppDate.getAbsolute(date);
 
   const dateResult =
     wholeMinutesDiff < 1 ? 'now' : wholeDaysDiff > 10 ? absolute : relative;
---
+++ src/utils/AppDate.ts
@@ -0,0 +1,16 @@
+import { DateTime, DurationLikeObject } from 'luxon';
+
+export const AppDate = {
+  getRelative: (date: string): string => {
+    return DateTime.fromISO(date).toRelative() as string;
+  },
+  getAbsolute: (date: string): string => {
+    return DateTime.fromISO(date).toLocaleString(DateTime.DATE_MED);
+  },
+  getFromNowDiffAs: (date: string, diffType: keyof DurationLikeObject): number => {
+    const duration = DateTime.fromJSDate(new Date()).diff(DateTime.fromISO(date));
+    const diff = duration.as(diffType);
+
+    return diff;
+  },
+};
