6ae865135ee51cf7a81e16db03ef451621ea4bb7
siriuszx <114768817+Siriuszx@users.noreply.github.com>
2024-03-10 13:32:14 +0100

    style: fix home page stutter

--- src/features/miscList/components/MiscContainer.styled.ts
+++ src/features/miscList/components/MiscContainer.styled.ts
@@ -1,9 +1,9 @@
 import styled from 'styled-components';
 
 export const Wrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 3rem;
+  display: grid;
+  grid-template-rows: repeat(3, minmax(auto, 275px));
+  gap: 2rem;
 
   padding: 3rem 2rem;
   border-left: 1px solid ${({ theme }) => theme.color.main_border};
