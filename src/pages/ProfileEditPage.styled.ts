9e2b5cf50607ac121b91b8432abb91f76d6a7cfc
siriuszx <114768817+Siriuszx@users.noreply.github.com>
2024-03-22 10:03:47 +0100

    style: profile edit page

--- src/pages/ProfileEditPage.styled.ts
+++ src/pages/ProfileEditPage.styled.ts
@@ -3,33 +3,44 @@ import { ErrorMessage } from '@/styles/components/ErrorMessage';
 import { FormButton } from '@/styles/components/FormButton';
 import { InputLabel } from '@/styles/components/InputLabel';
 import { InputText } from '@/styles/components/InputText';
+import AppImage from '@/features/appImages/components/AppImage';
+import { Username } from '@/styles/components/Username.styled';
 
 export const Wrapper = styled.div`
+  display: grid;
+  grid-template-columns: minmax(50ch, 80ch) minmax(30ch, 40ch);
+  justify-content: center;
+  gap: 4rem;
+
+  min-height: 100%;
+`;
+
+export const WrapperMain = styled.div`
   display: flex;
   flex-direction: column;
   gap: 2rem;
 
   padding: 3rem 0;
-`;
-
-export const Header = styled.h2`
-  font-family: ${({ theme }) => theme.font.times};
-  font-weight: 400;
-  text-align: center;
+  border-right: 1px solid ${({ theme }) => theme.color.main_border};
 `;
 
 export const Form = styled.form`
   display: flex;
   flex-direction: column;
-  gap: 2rem;
+  gap: 1rem;
   overflow-wrap: break-word;
+`;
+
+export const InputGroup = styled.div`
+  display: flex;
+  gap: 3rem;
 `;
 
 export const InputContainer = styled.div`
   display: flex;
-  align-items: center;
+  align-items: flex-start;
   flex-direction: column;
-  gap: 3rem;
+  gap: 2rem;
 `;
 
 export const InputItem = styled.div`
@@ -41,11 +52,11 @@ export const InputItem = styled.div`
 export const StyledInputLabel = styled(InputLabel)`
   font-size: 1rem;
   font-family: ${({ theme }) => theme.font.times};
-  text-align: center;
 `;
 
 export const StyledInputText = styled(InputText)`
   min-width: 300px;
+  text-align: start;
 `;
 
 export const StyledInputTextArea = styled.textarea`
@@ -73,4 +84,49 @@ export const ControlsContainer = styled.div`
   align-self: flex-end;
 `;
 
-export const SubmitButton = styled(FormButton)``;
+export const SubmitButton = styled(FormButton)`
+  align-self: flex-start;
+
+  padding-left: 3rem;
+  padding-right: 3rem;
+`;
+
+export const WrapperAside = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  padding: 3rem 0;
+`;
+
+export const ProfileWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+`;
+
+export const ProfileIcon = styled(AppImage)`
+  width: 88px;
+  height: 88px;
+  border-radius: 50%;
+`;
+
+export const StyledUserName = styled(Username)`
+  max-width: none;
+
+  white-space: break-spaces;
+  word-wrap: break-word;
+`;
+
+export const FollowCount = styled.span`
+  display: inline-block;
+
+  font-weight: 300;
+`;
+
+export const SignUpDate = styled.span`
+  display: inline-block;
+
+  font-size: 0.8rem;
+  font-weight: 300;
+  font-style: italic;
+`;
--- src/pages/ProfileEditPage.tsx
+++ src/pages/ProfileEditPage.tsx
@@ -2,7 +2,11 @@ import { useAppDispatch, useAppSelector } from '@/app/hooks';
 import { Navigate, useNavigate } from 'react-router-dom';
 
 import { selectIsAuthenticated } from '@/features/auth/authSlice';
-import { putProfileData, selectProfileData } from '@/features/profile/profileSlice';
+import {
+  putProfileData,
+  selectProfileData,
+  selectPutProfileDataState,
+} from '@/features/profile/profileSlice';
 
 import * as S from './ProfileEditPage.styled';
 import { useForm } from 'react-hook-form';
@@ -12,10 +16,13 @@ import {
 } from '@/types/formSchemas/ProfileEditSchema';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { StyledErrorMessage } from './CreatePost.styled';
+import { DateTime } from 'luxon';
 
 const ProfileEditPage = () => {
   const isAuthenticated = useAppSelector(selectIsAuthenticated);
+
   const profileData = useAppSelector(selectProfileData);
+  const { isLoading, error } = useAppSelector(selectPutProfileDataState);
 
   const {
     register,
@@ -37,65 +44,86 @@ const ProfileEditPage = () => {
   if (!isAuthenticated || !profileData) return <Navigate to="/" />;
 
   const handleFormSubmit = async (formData: TProfileEditSchema): Promise<void> => {
-    const response = await dispatch(putProfileData(formData)).unwrap();
+    if (!isLoading) {
+      const response = await dispatch(putProfileData(formData)).unwrap();
 
-    if (response) navigate('/');
+      if (response) navigate('/');
+    }
   };
+
+  const signUpDate = DateTime.fromISO(profileData?.sign_up_date as string).toLocaleString(
+    DateTime.DATE_MED
+  );
 
   return (
     <S.Wrapper>
-      <S.Header>{profileData?.username}</S.Header>
-      <S.Form onSubmit={handleSubmit(handleFormSubmit)}>
-        <S.InputContainer>
-          <S.InputItem>
-            <S.StyledInputLabel htmlFor="edit-username">Username</S.StyledInputLabel>
-            <S.StyledInputText
-              {...register('username', { required: 'Username is required' })}
-              id="edit-username"
-              type="text"
-            />
-            <StyledErrorMessage $isVisible={Boolean(errors.username)}>
-              {errors.username?.message}
+      <S.WrapperMain>
+        <S.Form onSubmit={handleSubmit(handleFormSubmit)}>
+          <S.InputContainer>
+            <S.InputGroup>
+              <S.InputItem>
+                <S.StyledInputLabel htmlFor="edit-username">Username</S.StyledInputLabel>
+                <S.StyledInputText
+                  {...register('username', { required: 'Username is required' })}
+                  id="edit-username"
+                  type="text"
+                />
+                <StyledErrorMessage $isVisible={Boolean(errors.username)}>
+                  {errors.username?.message}
+                </StyledErrorMessage>
+              </S.InputItem>
+              <S.InputItem>
+                <S.StyledInputLabel htmlFor="edit-email">Email</S.StyledInputLabel>
+                <S.StyledInputText
+                  {...register('email', { required: 'Email is required' })}
+                  id="edit-email"
+                  type="text"
+                />
+                <StyledErrorMessage $isVisible={Boolean(errors.email)}>
+                  {errors.email?.message}
+                </StyledErrorMessage>
+              </S.InputItem>
+            </S.InputGroup>
+            <S.InputItem>
+              <S.StyledInputLabel htmlFor="edit-password">Password</S.StyledInputLabel>
+              <S.StyledInputText
+                {...register('password', { required: 'Password is required' })}
+                id="edit-password"
+                type="password"
+                placeholder="••••••••"
+              />
+              <StyledErrorMessage $isVisible={Boolean(errors.password)}>
+                {errors.password?.message}
+              </StyledErrorMessage>
+            </S.InputItem>
+            <S.InputItem>
+              <S.StyledInputLabel htmlFor="edit-bio">Bio</S.StyledInputLabel>
+              <S.StyledInputTextArea
+                {...register('bio', { required: 'Bio is required' })}
+                id="edit-bio"
+                cols={45}
+                rows={5}
+              />
+            </S.InputItem>
+            <StyledErrorMessage $isVisible={Boolean(errors.bio)}>
+              {errors.bio?.message}
             </StyledErrorMessage>
-          </S.InputItem>
-          <S.InputItem>
-            <S.StyledInputLabel htmlFor="edit-email">Email</S.StyledInputLabel>
-            <S.StyledInputText
-              {...register('email', { required: 'Email is required' })}
-              id="edit-email"
-              type="text"
-            />
-            <StyledErrorMessage $isVisible={Boolean(errors.email)}>
-              {errors.email?.message}
-            </StyledErrorMessage>
-          </S.InputItem>
-          <S.InputItem>
-            <S.StyledInputLabel htmlFor="edit-password">Password</S.StyledInputLabel>
-            <S.StyledInputText
-              {...register('password', { required: 'Password is required' })}
-              id="edit-password"
-              type="password"
-              placeholder="••••••••"
-            />
-            <StyledErrorMessage $isVisible={Boolean(errors.password)}>
-              {errors.password?.message}
-            </StyledErrorMessage>
-          </S.InputItem>
-          <S.InputItem>
-            <S.StyledInputLabel htmlFor="edit-bio">Bio</S.StyledInputLabel>
-            <S.StyledInputTextArea
-              {...register('bio', { required: 'Bio is required' })}
-              id="edit-bio"
-              cols={45}
-              rows={5}
-            />
-          </S.InputItem>
-          <StyledErrorMessage $isVisible={Boolean(errors.bio)}>
-            {errors.bio?.message}
-          </StyledErrorMessage>
-        </S.InputContainer>
-        <S.SubmitButton type="submit" />
-      </S.Form>
+          </S.InputContainer>
+          <S.StyledErrorMessage $isVisible={Boolean(error)}>
+            An error has occurred while submitting the form
+          </S.StyledErrorMessage>
+          <S.SubmitButton type="submit" />
+        </S.Form>
+      </S.WrapperMain>
+      <S.WrapperAside>
+        <S.ProfileWrapper>
+          <S.ProfileIcon imageId={profileData.profile_image} altText="Profile Icon" />
+          <S.StyledUserName>{profileData?.username}</S.StyledUserName>
+          <S.FollowCount>{profileData?.followed_users.length} Following</S.FollowCount>
+          <S.FollowCount>{profileData?.users_following.length} Followers</S.FollowCount>
+          <S.SignUpDate>Member since: {signUpDate}</S.SignUpDate>
+        </S.ProfileWrapper>
+      </S.WrapperAside>
     </S.Wrapper>
   );
 };
