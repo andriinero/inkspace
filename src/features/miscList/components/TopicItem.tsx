822d1cceef18b0b0c00810665245522a6b19c7a8
siriuszx <114768817+Siriuszx@users.noreply.github.com>
2024-06-01 16:55:40 +0200

    chore: restore file

---
+++ src/features/miscList/components/TopicItem.tsx
@@ -0,0 +1,20 @@
+import { TopicButton, Wrapper } from './TopicItem.styled';
+
+type TopicItemProps = {
+  name: string;
+};
+
+const TopicItem = ({ name }: TopicItemProps) => {
+  return (
+    <Wrapper>
+      <TopicButton
+        disabled={true}
+        $isActive={false}
+        type="button"
+        value={name}
+      />
+    </Wrapper>
+  );
+};
+
+export default TopicItem;
