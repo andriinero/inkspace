06cec508c115eda6746c64bef0b898e37260ba14
siriuszx <114768817+Siriuszx@users.noreply.github.com>
2024-06-10 08:36:42 +0200

    fix: single page post loader

--- src/components/loaders/PostPageLoader.styled.ts
+++ src/components/loaders/PostPageLoader.styled.ts
@@ -5,17 +5,17 @@ export const Wrapper = styled.div`
   & > * {
     ${tw`animate-pulse bg-gray-50`}
   }
-  ${tw`flex flex-col gap-4 my-16 min-w-[70ch]`}
+  ${tw`flex flex-col gap-4 my-16 w-[40ch] sm:w-[70ch]`}
 `;
 
 export const Header = styled.div`
-  ${tw`min-h-[15ch]`}
+  ${tw`h-[15ch]`}
 `;
 
 export const MiscInfo = styled.div`
-  ${tw`min-h-[10ch]`}
+  ${tw`h-[10ch]`}
 `;
 
 export const Body = styled.div`
-  ${tw`min-h-[80ch]`}
+  ${tw`h-[50ch]`}
 `;
